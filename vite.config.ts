e58a05274a7d1557e670799c4da5bd99dfb11b2a
fangwang <2781216298@qq.com>
2025-04-23 23:30:51 +0800

    允许内网穿透

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  server: {
+    host: "0.0.0.0", // 确保监听所有地址，以便 ngrok 可以访问
+    port: 5173, // 确保端口与 ngrok 转发的端口一致
+    allowedHosts: [
+      "all", // 添加您的 ngrok 域名
+      // 如果您希望允许所有主机（在开发环境中更方便，但安全性稍低），可以使用 'all'
+      // 'all',
+    ],
+  },
 });
